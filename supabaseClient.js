29c3fb3c2a79e113567e8ce7d50644302b9043cf
Laca2001 <alonsolaca@gmail.com>
2025-12-04 14:43:14 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // Usa la variable global "window.supabase" que carga desde el CDN
-const SUPABASE_URL = "TU_PROJECT_URL_AQUI";
-const SUPABASE_ANON_KEY = "TU_ANON_PUBLIC_KEY_AQUI";
+const SUPABASE_URL = "https://euuwitoztqanqaerkiwg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1dXdpdG96dHFhbnFhZXJraXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzI5NjAsImV4cCI6MjA4MDQ0ODk2MH0.duRKMoYyT7AX3qi4Ot2AK6egNepi59yjlCZF7332SeY";
 
 // Creamos un cliente global que podrás usar en otros scripts
 window.supabaseClient = window.supabase.createClient(
